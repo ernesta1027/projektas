914538cc1784b3134baed2082a8b5f47d5294aa6
ernesta1027 <75785720+ernesta1027@users.noreply.github.com>
2022-01-13 20:35:45 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,19 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4></h4>
+    <p> </p> 
+</div>
+<div  class="col-lg-6">
+  
+
+  <h4>Apie svetainę</h4> 
+  <p> Tai svetainė, skirta studentams papildomam mokymuisi. Čia rasite įvairių pamokų ir užduočių žinių tobulinimui.
+  </p>
+      
+       <img src="images/education.jpg" style="width: 100%;object-fit: contain;height: 100%" alt="IMG">
+        
+</div>
+
+ 
+
